62d99cab21925dfbfff8346c9f620ff83b7c60eb
dgustavino <58349871+Dgustavino@users.noreply.github.com>
2021-04-26 11:14:46 -0600

    MVC

---
+++ Mvc_ASP/Mvc_ASP/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Mvc_ASP.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Mvc_ASP.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            var m = new MiModelo();
+            m.Nombre = "Pancho";
+            m.Apellidos = "Perez";
+            m.Edad = 30;
+            return View(m);
+        }
+
+    }
+}
---
+++ Mvc_ASP/Mvc_ASP/Models/MiModelo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Mvc_ASP.Models
+{
+    public class MiModelo
+    {
+        public string Nombre;
+        public string Apellidos;
+        public int Edad;
+    }
+}
